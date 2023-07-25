2e027f8b2aa6a74693d161eac6a2c517c2fbbce9
h5blog <h5blog@sina.com>
2023-07-25 09:49:22 +0800

    Add files via upload

---
+++ h5/apiConfig.js
@@ -0,0 +1,9 @@
+var apiHost = (function() {
+  //也可以根据web页面域名等条件返回相应的api域名，代码示例
+  // if(location.hostname==="product.yiliao.com"){
+  //   return 'api-product.yiliao.com'
+  // }else {
+  //   return 'http://113.200.100.190:50053'
+  // }
+  return 'http://113.200.100.190:50053'
+})();
